29ca6f98b92617ec6f4492a4fc238d87940932b8
gabrielgoncalves95 <gabriell.gonsalves95@gmail.com>
2019-05-11 02:08:35 -0300

    Adicionando exemplos de sintaxe

--- app.ts
+++ app.ts
@@ -1,3 +1,20 @@
-let message: string
-let variable: string
-let another: string
+let message: string = "Help me!";
+console.log(message);
+let episode: number = 4;
+console.log(episode+1);
+
+let isEnoughToBeatMF = function (parsecs: number): boolean{
+  return parsecs < 12
+}
+let distance: number = 14
+console.log(`Is ${distance} parsecs enough to beat? ${isEnoughToBeatMF(distance)}`)
+
+let call = (name: string) => console.log(`Do you copy, ${name}`)
+call("Gabs?")
+
+function increment(speed: number, inc: number = 1): number {
+  return speed+inc
+}
+
+console.log(`inc (5, 1) = ${increment(5, 1)}`)
+console.log(`inc (5) = ${increment(5)}`)
--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,17 @@
-var message;
-var variable;
-var another;
+var message = "Help me!";
+console.log(message);
+var episode = 4;
+console.log(episode + 1);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 14;
+console.log("Is " + distance + " parsecs enough to beat? " + isEnoughToBeatMF(distance));
+var call = function (name) { return console.log("Do you copy, " + name); };
+call("Gabs?");
+function increment(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5, 1) = " + increment(5, 1));
+console.log("inc (5) = " + increment(5));
